b09bf50ccd184a9734bd3384d8fa5bd5c988eb8e
Igor Edwards <101023683+igore91@users.noreply.github.com>
2022-03-29 11:09:31 -0300

    js

--- exercicios/030/javascript.js
+++ exercicios/030/javascript.js
@@ -1,2 +1,8 @@
 'use strict';
 
+var input1 = document.getElementById('input1')
+var input2 = document.getElementById('input2')
+var input3 = document.getElementById('input3')
+var input4 = document.getElementById('input4')
+var calcular = document.getElementById('calcular')
+var limpar = document.getElementById('limpar')
